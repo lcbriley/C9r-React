cd417837127df1ef8346a1249fb361a71c6f8215
lcbriley <70645338+lcbriley@users.noreply.github.com>
2022-06-16 16:05:19 -0400

    AddField validations

--- src/AddField.js
+++ src/AddField.js
@@ -12,6 +12,35 @@ function AddField(props) {
   const [buttonFieldGroupInfo, setButtonFieldGroupInfo] = useState(false);
   const [buttonFamilyTaggingInfo, setButtonFamilyTaggingInfo] = useState(false);
 
+  const [name, setName] = React.useState("");
+  const handleNameVal = evt => {
+    const nameVal = evt.target.value.replace(
+      /[#&]/g,
+      // /[^a-zA-Z\s0-9]/g,
+      ""
+    );
+    setName(nameVal);
+  };
+
+
+
+    const [showhide, setShowhide]=useState('');
+    
+     const handleshowhide=(event)=>{
+       const showGroup = event.target.value;    
+       setShowhide(showGroup);
+  
+     }
+     const [hideDisabled, setDisabled]=useState('');
+     
+     const handleDisabled=(event)=>{
+      const hideDisabled = event.target.value;    
+      setDisabled(hideDisabled);
+ 
+    }
+
+
+
     return (props.trigger) ? (
       <div className='popup fixed flex top-0 left-0 w-full h-screen justify-center items-center'>
       <div className='popup-inner relative bg-white w-full max-w-2xl '>
@@ -27,34 +56,38 @@ function AddField(props) {
             <div className='grid grid-cols-3 grid-rows-6 gap-3 mt-2 ml-4'>
 
                 <div className='col-span-1 flex text-sm'>Name: </div> 
-                <input type="text" className='col-span-2 text-sm mt-1 pl-1 appearance-none rounded block w-4/5 border border-grey-500 shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0 focus:drop-shadow-blue' ></input> 
+               <div className='col-span-2'>
+                  <input type="text" value={name} onChange={handleNameVal} className=' text-sm mt-1 pl-1 appearance-none rounded block w-4/5 border border-grey-500 shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0 focus:drop-shadow-blue' ></input> 
+                  
+               </div>
 
 
                 <div className='col-span-1 flex text-sm content-around'>Scope: </div>
-                <select className='col-span-2 text-sm mt-1 rounded block w-4/5 border border-grey-500 shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0'>
-                <option>Global</option>
-                <option>Local</option>
+                <select className='col-span-2 text-sm mt-1 rounded block w-4/5 border border-grey-500 shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0' onChange={(e)=>(handleDisabled(e))}>
+                <option value="0">Global</option>
+                <option value="a">Local</option>
                 </select>
 
                 <div className='col-span-1 flex text-sm content-around'>Field Type: <button onClick={() => setButtonFieldTypeInfo (true)} className='ml-1 text-dark-blue'><i class="fa-solid fa-circle-info"></i></button></div>
-                <select className='col-span-2 text-sm mt-1 rounded block w-4/5 border border-grey-500 shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0'>
-                  <option>Yes/No</option>
-                  <option>Text</option>
-                  <option>Date</option>
-                  <option>Number</option>
-                  <option>Select</option>
-                  <option>Multi-Select</option>
+                <select className='col-span-2 text-sm mt-1 rounded block w-4/5 border border-grey-500 shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0' onChange={(e)=>(handleshowhide(e))}>
+                  <option value="0">Yes/No</option>
+                  <option value="0">Text</option>
+                  <option value="0">Date</option>
+                  <option value="0">Number</option>
+                  <option value= "1">Select</option>
+                  <option value= "2">Multi-Select</option>
                 </select>
 
 
+
                 <div className='col-span-1 flex text-sm content-around'>Field Group: <button onClick={() => setButtonFieldGroupInfo (true)} className='ml-1 text-dark-blue'><i class="fa-solid fa-circle-info"></i></button> </div>
-                <select className='col-span-2 text-sm mt-1 rounded block w-4/5 border border-grey-500 shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0'>
+                <select className='col-span-2 text-sm mt-1 rounded block w-4/5 border border-grey-500 shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0' disabled= {hideDisabled === "a"}>
                   <option>Global</option>
                 </select>
 
                 
-                <input type="checkbox" id="quickSearch" name="quickSearch" value="quick" class="col-span-1 border-1 border-grey-300 shadow-sm h-3.5 w-3.5 mt-2" />
-                <div className='col-span-2 flex text-sm content-around'>Quick Search</div>
+             <input type="checkbox" id="quickSearch" name="quickSearch" value="quick" class="col-span-1 border-1 border-grey-300 shadow-sm h-3.5 w-3.5 mt-2" />
+              <div className='col-span-2 flex text-sm content-around'>Quick Search</div>
                
 
                 
@@ -64,6 +97,24 @@ function AddField(props) {
                 
                                            
             </div>
+
+             {
+              showhide >= "1" && <div className='mt-5'> 
+                <div className='font-semibold'>Select Options</div>
+                   <div className='place-content-center flex mt-2' >
+                     
+                        <div className='mt-1 text-sm'>Name:</div>
+                        <div className='ml-2 flex'>
+                          <input type="text" maxLength={250} className=' text-sm mt-1 pl-1 appearance-none rounded block border border-grey-500 shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0 focus:drop-shadow-blue' ></input>
+                          <button className='ml-2 rounded border border-grey-200 text-xs px-1 bg-gradient-to-r from-dark-blue to-light-blue text-white shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0'> Apply </button>
+                        </div>
+                        
+                     
+                   </div>
+                   <div className='text-xs text-gray-400'>(250 char max)</div>
+              </div> 
+            }
+
 
             <div className='flex justify-end mt-8'>
               <button onClick={() => props.setTrigger(false)} className='rounded border border-grey-200 py-1 px-2.5 mr-2 text-light-blue hover:text-dark-blue shadow-sm focus:outline-none focus:border-dark-blue focus:ring-dark-blue focus:ring-0'> Cancel </button>
